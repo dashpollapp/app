0143ee70fbe495d1b452cdd5b4e96b470df2be73
maxmlg <max@dashpoll.net>
2018-11-25 18:10:22 +0100

    [Fix] Require cycle gefixt & AUTH_LOGOUT fehlte in user_search.js

--- actions/load_polls_home.js
+++ actions/load_polls_home.js
@@ -1,8 +1,8 @@
 import { LOAD_POLLS_HOME, LOAD_POLLS_HOME_SUCCESS, LOAD_POLLS_HOME_FAIL } from "../constants/actionTypes";
-import { httpClient } from "../utils/store";
 
 export default function (skip, refresh = false) {
     const limit = /*store.getState().bl*/ 5;
+    const httpClient = require("../utils/store").httpClient;
     return dispatch => {
         dispatch({ type: LOAD_POLLS_HOME });
         httpClient.request({
