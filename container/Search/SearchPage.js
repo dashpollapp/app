01d806d4f50c8d013ea5b1979d5498b76c4c53bf
Leonard <luca@dashpoll.net>
2018-11-27 20:16:46 +0100

    [FE] search (temporary)

--- container/Search/SearchPage.js
+++ container/Search/SearchPage.js
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { View, TextInput, FlatList, StyleSheet, Text, TouchableOpacity } from "react-native";
 import React from "react";
 
 class SearchPage extends React.Component {
@@ -10,9 +10,51 @@ class SearchPage extends React.Component {
         return (
             <View>
                 
+                <TextInput placeholder="Username" style={css.input} />
+
+                <FlatList 
+                    style={css.list}
+                    data={[{username: "1"}, {username: "2"}, {username: "3"}, {username: "4"}, {username: "5"}]}
+                    keyExtractor={(item) => item.username}
+
+                    renderItem={({ item, index }) => {
+                
+                        return (
+                            <TouchableOpacity>
+                                <View style={css.user}>
+
+                                    <Text>{item.username}</Text>
+
+                                </View>
+                            </TouchableOpacity>
+                        )
+
+                    }}
+
+                />
+
             </View>
         );
     }
 }
+
+const css = StyleSheet.create({
+    input: {
+        width: "80%",
+        backgroundColor: "#fff",
+        alignSelf: 'center',
+        height: 40
+    },
+    list: {
+        width: "100%",
+        height: "100%"
+    },
+    user: {
+        backgroundColor: "#fff",
+        width: "100%",
+        height: 60,
+        margin: 10,
+    }
+});
 
 export default SearchPage;
