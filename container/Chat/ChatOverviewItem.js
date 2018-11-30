6e283f595d2e3c40d890c19ae9db1393cb6f0158
maxmlg <max@dashpoll.net>
2018-11-30 19:48:36 +0100

    .

--- container/Chat/ChatOverviewItem.js
+++ container/Chat/ChatOverviewItem.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { StatusBar, StyleSheet, Text, View, ScrollView, TouchableOpacity, Image, KeyboardAvoidingView, ImageBackground, Dimensions } from 'react-native';
 import { LinearGradient } from 'expo';
 import PropTypes from 'prop-types';
@@ -6,49 +6,47 @@ import PropTypes from 'prop-types';
 import { css } from "./ChatOverviewStyle"
 
 export default class ChatOverviewItem extends Component {
-  render() {
+    render() {
 
-    let data = this.props.data
-    console.log(data)
+        let data = this.props.data
+
+        let lastMessage = data.lastMessage.text
+        let title = data.title
+
+        let showAsNew = data.lastMessage.from != 0 && data.lastMessage.readed == 0
+        let priorityColor
+
+        switch (data.priority) {
+            case 1:
+                priorityColor = "#0ccc6c"
+                break;
+            case 2:
+                priorityColor = "#e0a83e"
+                break;
+            case 3:
+                priorityColor = "#db7a41"
+                break;
+            case 4:
+                priorityColor = "#d83a3a"
+                break;
+            default:
+                priorityColor = "#a3a3a3"
+                break;
+        }
+
+        return (
+            <View style={[css.chat, css.chatNew,]}>
+
+                {showAsNew ? <View style={[css.chatNewBox, { backgroundColor: priorityColor }]}></View> : null}
 
 
-    let lastMessage     = data.lastMessage.text
-    let title           = data.title
-
-    let showAsNew       = data.lastMessage.from != 0 && data.lastMessage.readed == 0
-    let priorityColor   
-
-    switch (data.priority) {
-        case 1:
-            priorityColor = "#0ccc6c" 
-            break;
-        case 2:
-            priorityColor = "#e0a83e"
-            break;
-        case 3:
-            priorityColor = "#db7a41"
-            break;
-        case 4:
-            priorityColor = "#d83a3a"
-            break;
-        default:
-            priorityColor = "#a3a3a3"
-            break;
+                <Image style={css.pb} source={require("../../assets/img/pb.png")} />
+                <View style={css.text}>
+                    <Text style={css.name}>{title}</Text>
+                    <Text numberOfLines={1} style={css.lastMessage}>{lastMessage}</Text>
+                </View>
+                <Text style={[css.sendTime, css.sendTimeNew, { color: "#e17055" }]}>12:14</Text>
+            </View>
+        )
     }
-
-    return (
-        <View style={[css.chat, css.chatNew,]}>	
-
-            {showAsNew ? <View style={[css.chatNewBox, {backgroundColor: priorityColor} ]}></View> : null}
-            
-             
-             <Image style={css.pb} source={require("../../assets/img/pb.png")}  />	
-             <View style={css.text}>	
-                 <Text style={css.name}>{title}</Text>	
-                 <Text numberOfLines={1} style={css.lastMessage}>{lastMessage}</Text>	
-             </View>	
-             <Text style={[css.sendTime, css.sendTimeNew, { color: "#e17055" }]}>12:14</Text>	
-         </View>	
-    )
-  }
 }
---
+++ utils/determineChangedProps.js
@@ -0,0 +1,27 @@
+//https://gist.github.com/sqren/780ae8ca1e2cf59050b0695c901b5aa3
+import React, { Component } from 'react';
+
+export default function withPropsChecker(WrappedComponent) {
+    return class PropsChecker extends Component {
+        componentWillReceiveProps(nextProps) {
+            Object.keys(nextProps)
+                .filter(key => {
+                    return nextProps[key] !== this.props[key];
+                })
+                .map(key => {
+                    console.log(
+                        'changed property:',
+                        key,
+                        'from',
+                        this.props[key],
+                        'to',
+                        nextProps[key]
+                    );
+                });
+        }
+        render() {
+            return <WrappedComponent {...this.props} />;
+        }
+    };
+}
+
