d1bb0d3d5db169dd4d234437908653b3a2b2dd04
Leonard <luca@dashpoll.net>
2018-12-07 21:55:55 +0100

    [Post] Post Component (anfang)

---
+++ components/Post.js
@@ -0,0 +1,72 @@
+import React from "react";
+
+import { Text, View, FlatList } from "react-native";
+import PropTypes from "prop-types";
+
+import s from "../container/Polls/PollStyle";
+import Author from "./Post/authorBox";
+import Description from "./Description";
+import PostTypes from "../container/Polls/PollTypes";
+
+class Post extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    checkFirst(index) {
+        if(index !== 0) return { borderTopWidth: 1, borderColor: "#eee" }
+    }
+
+    render() {
+
+        let { 
+            post,
+            extraData,
+            clickable,
+            scroll,
+            style
+        } = this.props;
+
+        if(!Array.isArray(post)) post = [post];
+
+        return(
+            <FlatList 
+                style={style}
+                data={post}
+                extraData={extraData}
+                scrollEnabled={scroll}
+                keyExtractor={(item) => item._id}
+                renderItem={({ item, index }) => {
+                    return(
+                        <View style={[s.post, this.checkFirst(index)]}>
+                            <View style={s.pPostHeader}>
+                                <Text style={s.pTitle}>{item.heading}</Text>
+                                <Author poll={item} clickable={clickable} navigation={this.props.navigation} />
+                            </View>
+                            <Description style={s.description} text={item.text} />
+                            <PostTypes poll={item} clickable={clickable} />
+                        </View>
+                    )
+                }}
+            />
+        )
+    }
+
+}
+
+Post.propTypes = {
+    post: PropTypes.oneOfType([
+        PropTypes.object,
+        PropTypes.array
+    ]),
+    extraData: PropTypes.oneOfType([
+        PropTypes.object,
+        PropTypes.array
+    ]),
+    style: PropTypes.object,
+    scroll: PropTypes.bool,
+    clickable: PropTypes.bool,
+}
+
+export default Post;
