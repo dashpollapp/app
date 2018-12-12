09858ce2e3c438aafff6aa59a39ab5581b8df5c1
maxmlg <max@dashpoll.net>
2018-12-12 21:32:33 +0100

    besser jz

--- actions/image_cache.js
+++ actions/image_cache.js
@@ -8,15 +8,15 @@ export default function (pb, loading) {
         dispatch({ type: actionTypes.IMAGE_CACHE, payload: { id: pb } });
 
         Expo.FileSystem.getInfoAsync(Expo.FileSystem.cacheDirectory + pb + ".jpg")
-        .then(data => {
-            if (data.exists) {
-                dispatch({ type: actionTypes.IMAGE_CACHE_SUCCESS, payload: { uri: data.uri, id: pb } })
-            } else {
-                Expo.FileSystem.downloadAsync("https://api.dashpoll.net/pb/" + pb, Expo.FileSystem.cacheDirectory + pb + ".jpg", { md5: true })
-                .then(file => {
-                    dispatch({ type: actionTypes.IMAGE_CACHE_SUCCESS, payload: { uri: file.uri, id: pb } })
-                })
-            }
-        })
+            .then(data => {
+                if (data.exists) {
+                    dispatch({ type: actionTypes.IMAGE_CACHE_SUCCESS, payload: { uri: data.uri, id: pb } })
+                } else {
+                    Expo.FileSystem.downloadAsync("https://api.dashpoll.net/pb/" + pb, Expo.FileSystem.cacheDirectory + pb + ".jpg", { md5: true })
+                        .then(file => {
+                            dispatch({ type: actionTypes.IMAGE_CACHE_SUCCESS, payload: { uri: file.uri, id: pb } })
+                        })
+                }
+            })
     }
 }
